f79402c2b6a202bb869a7b6593ec223dc303d314
Angel628455 <angel_morel@ucne.edu.do>
2026-03-11 18:42:23 -0400

    crear JugadoresResponseDto.kt

---
+++ app/src/main/java/edu/ucne/angel_morel_ap2_p2/data/remote/remotedatasource/JugadoresResponseDto.kt
@@ -0,0 +1,22 @@
+package edu.ucne.angel_morel_ap2_p2.data.remote.remotedatasource
+
+import edu.ucne.angel_morel_ap2_p2.domain.model.JugadoresCharacter
+
+data class CharacterResponseDto(
+    val items: List<CharacterDto>
+)
+
+data class CharacterDto(
+    val id: Int,
+    val name: String,
+    val ki: String,
+    val race: String,
+    val gender: String,
+    val description: String,
+    val image: String,
+    val maxKi: String,
+) {
+    fun toDomain() = JugadoresCharacter(
+        id, name, ki, race, gender, description, image, maxKi,
+    )
+}
