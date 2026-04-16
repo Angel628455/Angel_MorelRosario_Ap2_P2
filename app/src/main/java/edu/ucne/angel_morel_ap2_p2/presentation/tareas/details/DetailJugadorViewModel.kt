d790e00074e2672295d107f84c16ec37e137371f
Angel628455 <angel_morel@ucne.edu.do>
2026-04-16 07:41:55 -0400

    Arreglar Detail de JugadorViewModel

---
+++ app/src/main/java/edu/ucne/angel_morel_ap2_p2/presentation/tareas/details/DetailJugadorViewModel.kt
@@ -0,0 +1,54 @@
+package edu.ucne.angel_morel_ap2_p2.presentation.tareas.details
+//import javax.inject.Inject
+//import androidx.lifecycle.SavedStateHandle
+//import androidx.lifecycle.ViewModel
+//import androidx.lifecycle.viewModelScope
+//import androidx.navigation.toRoute
+//import dagger.hilt.android.lifecycle.HiltViewModel
+//import edu.ucne.angel_morel_ap2_p2.data.remote.remotedatasource.Resource
+//import edu.ucne.angel_morel_ap2_p2.presentation.navigation.Screen
+//import kotlinx.coroutines.flow.MutableStateFlow
+//import kotlinx.coroutines.flow.asStateFlow
+//import kotlinx.coroutines.flow.update
+//import kotlinx.coroutines.launch
+//
+//@HiltViewModel
+//class DetailJugadoresViewModel @Inject constructor(
+//    private val getJugadoresDetailUseCase: GetJugadoresDetailUseCase,
+//    savedStateHandle: SavedStateHandle
+//) : ViewModel() {
+//
+//    private val _state = MutableStateFlow(DetailJugadorUiState())
+//    val state = _state.asStateFlow()
+//
+//    init {
+//        val args = savedStateHandle.toRoute<Screen.JugadorDetail>()
+//        loadJugador(args.id)
+//    }
+//
+//    private fun loadJugador(id: Int) {
+//        viewModelScope.launch {
+//            _state.update { it.copy(isLoading = true) }
+//
+//            when (val result = getJugadorDetailUseCase(id)) {
+//                is Resource.Success ->
+//                    _state.update {
+//                        it.copy(
+//                            isLoading = false,
+//                            jugador = result.data
+//                        )
+//                    }
+//
+//                is Resource.Error ->
+//                    _state.update {
+//                        it.copy(
+//                            isLoading = false,
+//                            error = result.message
+//                        )
+//                    }
+//
+//                is Resource.Loading -> Unit
+//            }
+//        }
+//    }
+//}
