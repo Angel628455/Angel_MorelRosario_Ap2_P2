d033bae8a64d57ca13568bd536fdaf8c2cb8d91a
Angel628455 <angel_morel@ucne.edu.do>
2026-04-16 07:59:32 -0400

    Agregar Screen

---
+++ app/src/main/java/edu/ucne/angel_morel_ap2_p2/presentation/navigation/Screen.kt
@@ -0,0 +1,12 @@
+package edu.ucne.angel_morel_ap2_p2.presentation.navigation
+
+
+//import kotlinx.serialization.Serializable
+//
+//sealed class Screen{
+//    @Serializable
+//    data object JugadorList : Screen()
+//
+//    @Serializable
+//    data class JugadorDetail(val id: Int) : Screen()
+//}
