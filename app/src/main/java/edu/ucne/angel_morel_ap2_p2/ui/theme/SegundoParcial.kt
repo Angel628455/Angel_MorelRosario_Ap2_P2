e871545ed0aca88c7c874f4a829f990d44807c71
Angel628455 <angel_morel@ucne.edu.do>
2026-04-17 11:15:58 -0400

    Agregar SegundoParcial.kt

---
+++ app/src/main/java/edu/ucne/angel_morel_ap2_p2/ui/theme/SegundoParcial.kt
@@ -0,0 +1,8 @@
+package edu.ucne.angel_morel_ap2_p2.ui.theme
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SegundoParcialApp : Application() {
+}
